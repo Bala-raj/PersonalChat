6d52b038c358ec18ee8fff7593db7d63f50a08fd
Bala <balachander.r.j@gmail.com>
2017-05-23 22:57:01 +0530

    Fixed the font issue

--- src/login/login.js
+++ src/login/login.js
@@ -2,16 +2,20 @@ import React, { Component, PropTypes } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
-import { Text } from 'react-native';
+
+import { Screen, Title, Text, Divider, Button, Spinner } from '@shoutem/ui';
+
+import Input from '../components/Input';
+import LoginButton from './login-button';
 
 @connect(state => ({
-  user: state.user,
+  authorizing: state.user.authorizing,
 }),
     dispatch => bindActionCreators({ }, dispatch),
 )
-export default class Login extends Component {
+export default class LoginInterface extends Component {
   static propTypes = {
-    user: PropTypes.object.isRequired,
+    authorizing: PropTypes.bool.isRequired,
   }
 
   constructor() {
@@ -23,7 +27,20 @@ export default class Login extends Component {
 
   render() {
     return (
-     <Text> Hello! </Text>
+      <Screen style={{ alignItems: 'center', justifyContent: 'center' }}>
+        <Title>Who are you?</Title>
+        <Divider />
+
+        <Input
+          placeholder="Your name here"
+          submitAction
+          noclear
+          ref="username"
+        />
+        <Divider />
+
+        {this.props.authorizing ? <Spinner /> : <LoginButton />}
+      </Screen>
     );
   }
 }
--- src/setup/configureStore.js
+++ src/setup/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware, { END } from 'redux-saga';
 
+import { composeWithDevTools } from 'remote-redux-devtools';
+
 import rootReducer from './root-reducer';
 
 export default function configureStore() {
@@ -8,7 +10,7 @@ export default function configureStore() {
 
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // eslint-disable-line
 
-  const store = createStore(rootReducer, composeEnhancers(applyMiddleware(sagaMiddleware)));
+  const store = createStore(rootReducer, composeWithDevTools(composeEnhancers(applyMiddleware(sagaMiddleware))));
 
   store.runSaga = sagaMiddleware.run;
   store.close = () => store.dispatch(END);
